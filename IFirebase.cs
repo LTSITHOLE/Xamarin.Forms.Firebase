3aaba4facfae79d915c4a996beda3673a9d25440
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2021-03-16 05:47:52 +0200

    IFirebase Interface added

---
+++ IFirebase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.Firebase
+{
+    public interface IFirebase<T>
+    {
+        Task<bool> CreateObject(T Object);
+        Task<bool> DeleteObject(T Object);
+    }
+}
