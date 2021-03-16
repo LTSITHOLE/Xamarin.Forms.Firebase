cf40a45cb37c8f0b1dc9baa3df79de07992739b1
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2021-03-16 06:08:39 +0200

    Added SeverConfiguration

---
+++ src/ServerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.Firebase.src
+{
+    public static class ServerConfiguration
+    {
+        public static string FirebaseDatabaseURL { get; set; }
+        public static string FirebaseStorageURL { get; set; }
+        public static string FirebaseAuthKey { get; set; }
+    }
+}
